1580091b8627addb7d8a34472f62d33dff56efb0
Nikolay Martynov <nmartynov@shutterstock.com>
2013-09-18 17:26:54 -0400

    Version number increase

--- lib/Plack/Middleware/Timed/Logger.pm
+++ lib/Plack/Middleware/Timed/Logger.pm
@@ -13,11 +13,11 @@ Plack::Middleware::Timed::Logger - Expose a Timed::Logger Instance in Middleware
 
 =head1 VERSION
 
-Version 0.0.1
+Version 0.0.2
 
 =cut
 
-our $VERSION = '0.0.1';
+our $VERSION = '0.0.2';
 
 =head1 SYNOPSIS
 
