377d1b7ff288b9b0ea9770200ff2f2f0ddabd2e0
Nikolay Martynov <nmartynov@shutterstock.com>
2013-09-18 18:46:41 -0400

    Up version for cpan upload

--- lib/Plack/Middleware/Timed/Logger.pm
+++ lib/Plack/Middleware/Timed/Logger.pm
@@ -13,11 +13,11 @@ Plack::Middleware::Timed::Logger - Expose a Timed::Logger Instance in Middleware
 
 =head1 VERSION
 
-Version 0.0.2
+Version 0.0.3
 
 =cut
 
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 =head1 SYNOPSIS
 
